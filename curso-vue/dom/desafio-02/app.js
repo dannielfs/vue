f8dd0a682ff26a65df33a35e2c657cd4185dce65
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-22 15:21:10 -0300

    alteracoes

--- curso-vue/dom/desafio-02/app.js
+++ curso-vue/dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        clicar() {
+            return alert('teste')
+        },
+        setVal(event) {
+            this.valor = event.target.value
+        }
     }
 })
