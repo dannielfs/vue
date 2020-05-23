9dbae12e7d8944a2157f33ff89f07b14c16de8c2
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-23 19:22:28 -0300

    finalizando modulo 1

--- curso-vue/dom/desafio-04/app.js
+++ curso-vue/dom/desafio-04/app.js
@@ -1,14 +1,41 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+    classe: 'encolher',
+    minhasClasses: 'destaque',
+    width: 0,
+    classe1: true,
+    classe4: '',
+    perigo: true,
+    cor5: '',
+    estilo5: {
+      width: '100px',
+      height: '100px'
+    }
 	},
 	methods: {
 		iniciarEfeito() {
-
+      setTimeout(() => {
+        return this.classe = this.classe === 'encolher' ? 'destaque' : 'encolher'
+      }, 2000);
 		},
 		iniciarProgresso() {
+      let valor = 0;
+      const temporizador = setInterval(() => {
+        valor += 5;
+        this.width = `${valor}%`
+        if (valor == 100) {
+          clearInterval(temporizador);
+        }
+      }, 1000);
 
-		}
-	}
+    },
+    setPerigo(event) {
+      if(event.target.value == 'true') {
+        this.perigo = true;
+      } else if (event.target.value == 'false') {
+        this.perigo = false;
+      }
+    }
+  },
 })
