19f373800680a5d93fc0adba42c1199234db7537
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-24 19:09:36 -0300

    Ajustes nos ataques

---
+++ curso-vue/03-projeto-01-matador-de-monstros/app.js
@@ -0,0 +1,40 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerLife: 100,
+    monsterLife: 0,
+    running: false
+  },
+  computed: {
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0
+    }
+  },
+  methods: {
+    startGame() {
+      this.running = true;
+      this.playerLife = 100;
+      this.monsterLife = 100;
+    },
+
+    attack(especial) {
+      this.hurt('playerLife', 5, 10, especial);
+      this.hurt('monsterLife', 7, 12, false);
+    },
+
+    hurt(prop, min, max, especial) {
+      const plus = especial ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[prop] = Math.max(this.playerLife - hurt, 0)
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
+    }
+  },
+  watch: {
+    hasResult(value) {
+      if (value) this.running = false;
+    }
+  }
+})
