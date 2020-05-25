1c02871fac8a96f35b39df20d1afc7afd92f6c4e
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-25 09:42:06 -0300

    alteraçoes nos componentes

---
+++ curso-vue/07-componentes/componente-desafio/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-app>
+
+    <app-nav/>
+
+    <app-toolbar/>
+
+    <app-content/>
+
+    <app-footer/>
+
+	</v-app>
+</template>
+
+<script>
+
+import Content from './Content';
+import Nav from './Nav';
+import ToolBar from './ToolBar';
+import Footer from './Footer';
+
+export default {
+  components: {
+    'app-content': Content,
+    'app-nav': Nav,
+    'app-toolbar': ToolBar,
+    'app-footer': Footer
+  }
+}
+</script>
