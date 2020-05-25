007bd1377efbfe87b94da35f35d980bdf9d82412
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-25 00:04:46 -0300

    criando componente contador

---
+++ curso-vue/07-componentes/componente-intro/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contador/>
+    <app-contador/>
+    <app-contador/>
+  </div>
+</template>
+
+<style lang="stylus">
+
+</style>
+
+<script>
+export default {
+}
+</script>
