ebd7a9208f8eed07dc58cba58acfa82319fa622d
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-26 11:51:59 -0300

    Trabalhando com rotas

---
+++ curso-vue/16-rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="usuario">
+    <h2>Usuário</h2>
+    <hr>
+    <p><strong>Código:</strong> {{ id }}</p>
+    <br>
+    <button sucesso @click="irParaInicio()"> Voltar</button>
+    <hr>
+    <button sucesso @click="irParaInicioAssim()"> Voltar Assim</button>
+  </div>
+</template>
+<script>
+export default {
+    pros: [ 'id' ],
+/*     data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
+    watch: {
+      $route(to, from) {
+        this.id = to.params.id
+      }
+    }, */
+    methods: {
+      irParaInicio() {
+        this.$router.push('/' )
+      },
+      irParaInicioAssim() {
+        this.$router.push({ path: '/' })
+      }
+    }
+}
+</script>
