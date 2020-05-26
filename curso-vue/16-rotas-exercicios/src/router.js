5ce7b1f85975620cd2730f8fbf644fecd96b33d3
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-26 12:06:35 -0300

    ajustes para rotas aninhadas

--- curso-vue/16-rotas-exercicios/src/router.js
+++ curso-vue/16-rotas-exercicios/src/router.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Inicio from './components/Inicio';
 import Usuario from './components/usuario/Usuario'
+import UsuarioLista from './components/usuario/UsuarioLista'
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe'
+import UsuarioEditar from './components/usuario/UsuarioEditar'
 
 
 Vue.use(Router)
@@ -9,7 +12,30 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', component: Inicio },
-    { path: '/usuario/:id', component: Usuario, props: true }
+    {
+      path: '/',
+      component: Inicio
+    },
+    {
+      path: '/usuario',
+      component: Usuario,
+      props: true,
+      children: [
+        {
+          path: '',
+          component: UsuarioLista
+        },
+        {
+          path: ':id',
+          component: UsuarioDetalhe,
+          props: true
+        },
+        {
+          path: ':id/editar',
+          component: UsuarioEditar,
+          props: true
+        }
+      ]
+    }
   ]
 })
