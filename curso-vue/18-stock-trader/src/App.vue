e682657a41e58ec1a3eb5f9c0ba5ad947f3c1bc6
Danniel Sousa <danniel.sousa@gmail.com>
2020-05-31 21:04:50 -0300

    Alteraçoes no stocks

--- curso-vue/18-stock-trader/src/App.vue
+++ curso-vue/18-stock-trader/src/App.vue
@@ -13,7 +13,6 @@
 import Header from "./components/Header";
 
 export default {
-
   components: { Header }
 };
 </script>
--- curso-vue/18-stock-trader/src/components/stocks/Stocks.vue
+++ curso-vue/18-stock-trader/src/components/stocks/Stocks.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <stock v-for="stock in stocks" :key="stock.id"> </stock>
+  </v-layout>
 </template>
 
 <script>
 
+import Stock from './Stock'
+
+export default {
+  components: { Stock },
+  data: () => ({
+    stocks: [
+      { id: 1 , name: 'BMW', price: 110},
+      { id: 2 , name: 'Google', price: 200},
+      { id: 3 , name: 'Twiiter', price: 280},
+      { id: 4 , name: 'Apple', price: 83}
+    ]
+  })
+}
 </script>
 
 <style>
